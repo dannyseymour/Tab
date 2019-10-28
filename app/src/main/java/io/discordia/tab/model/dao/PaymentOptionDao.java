f90ac3e8ff44e2739d06b7b428c05650f371a38b
dannyseymour <daniel@discordia.io>
2019-10-28 05:48:34 -0600

    edited payment option Dao

--- app/src/main/java/io/discordia/tab/model/dao/PaymentOptionDao.java
+++ app/src/main/java/io/discordia/tab/model/dao/PaymentOptionDao.java
@@ -5,6 +5,7 @@ import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
+import io.discordia.tab.model.entity.Account;
 import io.discordia.tab.model.entity.PaymentOption;
 import java.util.Collection;
 import java.util.List;
@@ -16,6 +17,15 @@ public interface PaymentOptionDao {
 
   @Query("SELECT * FROM PaymentOption ORDER BY payment_option_id")
   LiveData<PaymentOption> getAllById();
+
+  @Query("SELECT * FROM PaymentOption ORDER BY payment_option_id ASC")
+  LiveData<List<Account>> getAll();
+
+  @Query("SELECT * FROM PaymentOption WHERE payment_option_id= :paymentOptionId")
+  LiveData<Account> getById(int paymentOptionId);
+
+  @Query("SELECT account_type FROM PaymentOption WHERE payment_option_id :paymentOptionId")
+  int getTypeOfOptionById(int paymentOptionId);
 
 
   @Update
